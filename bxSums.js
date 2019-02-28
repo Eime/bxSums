4dfebd6ac2ae7689ce1724d66a54bfbe4e6aee28
Michael Einberger <einberger@mobimedia.de>
2019-02-28 15:14:46 +0100

    Ausgeblendete Karten durch Scrolling nachladen
    Zudem kann nun nach Karten gefiltert werden,
    indem man oben auf die Person klickt.

--- bxSums.js
+++ bxSums.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bitrix-Sums
-// @version      1.3
+// @version      1.4
 // @description  Summiert die Stunden in Bitrix-Boards
 // @author       Michael E.
 // @updateURL    https://eime.github.io/bxSums/bxSums.meta.js
@@ -20,9 +20,22 @@
     );
 
     if (myJQuery(".main-kanban-column").length) {
-        calcEstimations();
+        var
+          $columns = myJQuery(".main-kanban-column-body"),
+          allLoaded = _.after($columns.length, function () {
+              window.setInterval(calcEstimations, 2000);
+          });
 
-        window.setInterval(calcEstimations, 2000);
+        // Wir muessen alle Columns scrollen, um sicherzugehen dass alle Karten geladen sind:
+        myJQuery(".main-kanban-column-body").each(function () {
+            var $col = myJQuery(this);
+            $col.scrollTop(50000);
+            _.delay(function () {
+                $col.scrollTop(0);
+                allLoaded();
+            });
+        });
+
     }
 
     // Beim Klick auf den Titel einer Liste werden alle Karten darin in neuen Tabs geoeffnet
@@ -36,8 +49,9 @@
 function calcEstimations() {
     myJQuery(".main-kanban-column").each(function () {
         var
-            $target = myJQuery(this).find(".customBxSums"),
-            $cards = myJQuery(this).find(".main-kanban-item"),
+            $column = myJQuery(this),
+            $target = $column.find(".customBxSums"),
+            $cards = $column.find(".main-kanban-item"),
             totalDonesAll = 0,
             totalEstimationsAll = 0,
             totalDones = {},
@@ -84,6 +98,10 @@ function calcEstimations() {
                 myJQuery("<li>")
                     .addClass("uSum")
                     .append($responsible.clone())
+                    .bind("click", function () {
+                        filterResponsible(myJQuery(this), responsible);
+                    })
+                    .toggleClass("filtered", responsible === $column.attr("rel"))
                     .append(
                         myJQuery("<div>").text(
                             formatTime(totalDones[responsible] || 0) +
@@ -96,6 +114,9 @@ function calcEstimations() {
                 $target.css("display", "flex");
                 myJQuery("<li>")
                     .addClass("totals")
+                    .bind("click", function () {
+                        filterResponsible(myJQuery(this), null);
+                    })
                 .append(
                     myJQuery("<div>")
                     .html('<svg viewBox="0 0 24 24"><path fill="#000000" d="M12,6A3,3 0 0,0 9,9A3,3 0 0,0 12,12A3,3 0 0,0 15,9A3,3 0 0,0 12,6M6,8.17A2.5,2.5 0 0,0 3.5,10.67A2.5,2.5 0 0,0 6,13.17C6.88,13.17 7.65,12.71 8.09,12.03C7.42,11.18 7,10.15 7,9C7,8.8 7,8.6 7.04,8.4C6.72,8.25 6.37,8.17 6,8.17M18,8.17C17.63,8.17 17.28,8.25 16.96,8.4C17,8.6 17,8.8 17,9C17,10.15 16.58,11.18 15.91,12.03C16.35,12.71 17.12,13.17 18,13.17A2.5,2.5 0 0,0 20.5,10.67A2.5,2.5 0 0,0 18,8.17M12,14C10,14 6,15 6,17V19H18V17C18,15 14,14 12,14M4.67,14.97C3,15.26 1,16.04 1,17.33V19H4V17C4,16.22 4.29,15.53 4.67,14.97M19.33,14.97C19.71,15.53 20,16.22 20,17V19H23V17.33C23,16.04 21,15.26 19.33,14.97Z" /></svg>')
@@ -108,6 +129,28 @@ function calcEstimations() {
             }
     });
 }
+
+function filterResponsible ($li, responsible) {
+    var
+        $column = $li.parents(".main-kanban-column"),
+        $items = $column.find(".main-kanban-item"),
+        curFilter = $column.attr("rel"),
+        newFilter = responsible;
+
+    if (!responsible || curFilter === responsible) {
+        newFilter = null;
+    }
+
+    $items.each(function () {
+        myJQuery(this).toggle(
+            !newFilter || responsible === myJQuery(this).find(".tasks-kanban-item-responsible .tasks-kanban-item-author-avatar").attr("title")
+        );
+    });
+    myJQuery(".customBxSums li").removeClass("filtered");
+    $li.toggleClass("filtered", !!newFilter);
+    $column.attr("rel", newFilter);
+}
+
 
 function formatTime (totalSeconds) {
     var
