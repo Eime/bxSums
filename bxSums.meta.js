d8dacc615d453d12a77944c2bfd0a5eb6d1659ab
Eime <michael.einberger@gmx.de>
2024-01-25 10:31:34 +0100

    Update bxSums.meta.js

--- bxSums.meta.js
+++ bxSums.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bitrix-Sums
-// @version      2.19
+// @version      2.20
 // @description  Summiert die Stunden in Bitrix-Boards
 // @author       Michael E.
 // @updateURL    https://eime.github.io/bxSums/bxSums.meta.js
