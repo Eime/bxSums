5494299e75c03dfbdcd0f6bb6dccfd2b5d54832f
Felix Moschenski <164498961+FelixMosch@users.noreply.github.com>
2025-10-07 14:37:27 +0200

    Update bxSums.meta.js

--- bxSums.meta.js
+++ bxSums.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bitrix-Sums
-// @version      2.32
+// @version      2.33
 // @description  Summiert die Stunden in Bitrix-Boards
 // @author       Michael E.
 // @updateURL    https://eime.github.io/bxSums/bxSums.meta.js
