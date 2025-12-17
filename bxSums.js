97b99f16b56617712d9b83e538b9d7f6212002c0
Eime <michael.einberger@gmx.de>
2025-12-17 10:19:48 +0100

    Update bxSums.js

--- bxSums.js
+++ bxSums.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bitrix-Sums
-// @version      2.39
+// @version      2.40
 // @description  Summiert Stunden und Story Points in Bitrix-Boards und Sprints (mit Rest-Tags Unterstützung)
 // @author       Michael E.
 // @updateURL    https://eime.github.io/bxSums/bxSums.meta.js
@@ -34,7 +34,7 @@ var
     );
 //     if (_$(".main-kanban-column").length || _$("#bizproc_task_list_table").length || _$(".tasks-iframe-header").length || _$(".tasks-scrum__scope").length) {
 //         _$("head").append(
-//             '<link id="bxSumsLink" href="https://eime.github.io/bxSums/bxSumsCards.css?36" rel="stylesheet" type="text/css">'
+//             '<link id="bxSumsLink" href="https://eime.github.io/bxSums/bxSumsCards.css?40" rel="stylesheet" type="text/css">'
 //         );
 //     }
 
